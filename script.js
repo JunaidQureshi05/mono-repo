bc12fa17027cde02a92798bc2571bb3411ab16ba
Juned Ahmad <juned.amhad@myparkplus.com>
2024-03-18 12:56:13 +0530

    .

--- script.js
+++ script.js
@@ -1,68 +1,62 @@
 let container = document.querySelector(".container");
+let footerText = document.getElementById("footer-text");
 
-let names = [
-  "Form Validator",
-  "New Year Countdown",
-  "Video Player",
-  "Exchange Rate Calculator",
-  "Movie Seat Booking",
-  "DOM Array Methods",
-  "Lyrics Search",
-  "Break Out Game",
-  "Memory Cards",
-  "Infinite Scroll",
-  "Number Guessing Game",
-  "Speech Text Reader",
-  "Hangman Game",
-  "Landing Page",
-  "Drag and Drop",
-  "Speed Typer",
-  "Relaxer",
-  "Meal Finder",
-  "Music Player",
-  "Expense Tracker",
-];
-let links = [
-  "./projects/form-validator",
-  "./projects/new-year-countdown",
-  "./projects/custom-video-player",
-  "./projects/exchange-rate-calculator",
-  "./projects/movie-booking",
-  "./projects/dom-array-method",
-  "./projects/lyrics-search",
-  "./break-out-game.html",
-  "./projects/memory-cards",
-  "./projects/infinite-scroll",
-  "./projects/speak-number-guessing",
-  "./projects/speech-text-reader",
-  "./projects/hangman-game",
-  "./projects/menu-slider-and-modal",
-  "./projects/sortable-list",
-  "./projects/typing-game",
-  "./projects/relaxer",
-  "./projects/meal-finder",
-  "./projects/music-player",
-  "./projects/expense-tracker",
+let projects = [
+  { name: "Form Validator", link: "form-validator", image: "1.png" },
+  { name: "New Year Countdown", link: "new-year-countdown", image: "2.png" },
+  { name: "Custom Video Player", link: "custom-video-player", image: "3.png" },
+  {
+    name: "Exchange Rate Calculator",
+    link: "exchange-rate-calculator",
+    image: "4.png",
+  },
+  { name: "Movie Booking", link: "movie-booking", image: "5.png" },
+  { name: "DOM Array Method", link: "dom-array-method", image: "6.png" },
+  { name: "Lyrics Search", link: "lyrics-search", image: "7.png" },
+  { name: "Break Out Game", link: "break-out-game", image: "8.png" },
+  { name: "Memory Cards", link: "memory-cards", image: "9.png" },
+  { name: "Infinite Scroll", link: "infinite-scroll", image: "10.png" },
+  {
+    name: "Speak Number Guessing",
+    link: "speak-number-guessing",
+    image: "11.png",
+  },
+  { name: "Speech Text Reader", link: "speech-text-reader", image: "12.png" },
+  { name: "Hangman Game", link: "hangman-game", image: "13.png" },
+  {
+    name: "Menu Slider and Modal",
+    link: "menu-slider-and-modal",
+    image: "14.png",
+  },
+  { name: "Sortable List", link: "sortable-list", image: "15.png" },
+  { name: "Typing Game", link: "typing-game", image: "16.png" },
+  { name: "Relaxer", link: "relaxer", image: "17.png" },
+  { name: "Meal Finder", link: "meal-finder", image: "18.png" },
+  { name: "Music Player", link: "music-player", image: "19.png" },
+  { name: "Expense Tracker", link: "expense-tracker", image: "20.png" },
 ];
 
-function createBox(link, no) {
-  let box = document.createElement("div");
+function createBox(project) {
+  let box = document.createElement("a");
   box.classList.add("project");
-  box.style.backgroundImage = `url('./img/${no}.png')`;
+  box.href = `./projects/${project.link}`;
 
-  box.innerHTML = `
-  
-  <div class="content">
-  <div class="title">${names[no - 1]}</div>
-  <a href="${link}" class="btn">See Project</a>
-</div>
+  let image = document.createElement("div");
+  image.classList.add("image");
+  image.style.backgroundImage = `url('./img/${project.image}')`;
+
+  let content = document.createElement("div");
+  content.classList.add("content");
+  content.innerHTML = `
+    <div class="title">${project.name}</div>
   `;
+
+  box.appendChild(image);
+  box.appendChild(content);
+
   return box;
 }
 
-links.forEach((link, idx) => {
-  container.appendChild(createBox(link, idx + 1));
-});
-console.log(container);
+projects.forEach((project) => container.appendChild(createBox(project)));
 
-``;
+footerText.innerText = `All rights reserved - Juned Ahmad ${new Date().getFullYear()}`;
